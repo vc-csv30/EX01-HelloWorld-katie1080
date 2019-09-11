97812e55cab5df52886fb8cf32aa6233e75e8053
PaigeV <paige_vasquezhousle1@my.vcccd.edu>
2019-09-11 11:23:10 -0700

    hsdgfgjkher

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,18 @@
-//
-// Created by aknight on 8/2/19.
-//
+/*
+ * CS V30 Beginning C++
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have copied all of this
+ * code and I have not written this code.
+ *
+ * @Katie Woodson -- katie1080
+ */
 
+#include <iostream>
+#include <csv30>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
